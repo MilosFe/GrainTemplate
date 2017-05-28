431ae08fff19ba68979e8e4c8e99799a479457b6
TensaZangetsu <l.lawliet46@gmail.com>
2017-05-28 13:28:00 +0200

    inital commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/app'));
-app.set('port', (process.env.PORT || 6000));
+app.set('port', (process.env.PORT || 5000));
 app.set('app', __dirname + '/app');
 
 
