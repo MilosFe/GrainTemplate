e3d736fb945b7fe5eff0c9f3ef8cd7f4b15887ae
TensaZangetsu <l.lawliet46@gmail.com>
2017-05-28 13:15:59 +0200

    Heroku Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var request = require("request");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/app'));
+app.set('port', (process.env.PORT || 5000));
+app.set('app', __dirname + '/app');
+
+
+
+app.get('/', function(request, result) {
+    result.send('index');
+})
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on', app.get('port'));
+});
